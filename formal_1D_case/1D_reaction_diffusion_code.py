ba533e606a74d5ab7a55165c2770bd55f4603278
bunny <kawaiiitzuyu@gmail.com>
2025-02-14 00:37:26 +0000

    work continued

--- formal_1D_case/1D_reaction_diffusion_code.py
+++ formal_1D_case/1D_reaction_diffusion_code.py
@@ -3,12 +3,26 @@ import scipy as sp
 
 # Crank-Nicholson approach to solving reaction diffusion equation in 1 dimension iterating
 
+# Function as in Rich's work for rescaling the coordinates in the metal region to resolve at the interface
+def x_from_X(X):
+    """Defines the (non-uniformly distributed) physical coordinate 'x'
+    in terms of the (uniformly distributed) computational coordinate
+    'X'. Returns: not only x(X) but also x'(X) and x''(X).
+
+    """
+    BX = 0.01  # smaller BX leads to more NON-uniformity
+    # +0*X below prevents numpy from returning a scalar when acting
+    # on an iterable container for X
+    return (X + BX) * (X + BX) - BX * BX, 2 * (X + BX)
+
 # oxide-mesh
 n3 = 51
 # metal-mesh
 n2 = 101
 # Value to skip between rows of matrix
 ej = 2
+
+
 
 
 
@@ -55,6 +69,22 @@ c2 = np.zeros(n2)
 
 # array of uranium concentrations
 alpha = np.zeros(n2)
+
+# array of diffusivities
+D = D2 * np.ones(n2)
+
+# Choose computation coord Xmax such that x_from_X(Xmax)=L2
+Xmax = sp.optimize.root(lambda X: x_from_X(X)[0] - L2, 5).x[0]
+# exit if sanity check fails
+assert abs(x_from_X(Xmax)[0] - L2) < 1.0e-8
+# nodes in the oxide and bulk
+x3_nodes = np.linspace(-L3, 0, n3)  # oxide, the interface is at x=0
+X2_nodes = np.linspace(0, Xmax, n2)  # bulk nodes in comp. coordinate
+# physical node locations
+x2_nodes, x2d_nodes = x_from_X(X2_nodes)
+
+
+
 
 cs = Csstar / Castar # solubility limit
 eps = Castar / N2star
@@ -114,4 +144,38 @@ while t < t_max:
         b += [c3[n3-1] - c2[0]]
 
         # interface matching gradients (Fick's second law)
-        i = n3
+        i = n3 
+
+        D[0] = D1 * (1-alpha) + D2 * alpha
+        row += 6 * [i]
+        col += [i-3,i-2,i-1,i,i+ej,i+2*ej] # Either side of the boundary stencil
+        val += [-D3 / (2 * h3),
+                +4 * D3 / (2 * h3), # oxide boundary 
+                -3 * D3 / (2 * h3),
+                
+                -3 * D[0] / (2 * h2 * x2d_nodes[0]),        
+                4 * D[0] / (2 * h2 * x2d_nodes[0]), # metal/hydride boundary
+                -D[0] / (2 * h2 * x2d_nodes[0])]
+        b += [ D3 * (3 * c3[n3 - 1] - 4 * c3[n3 - 2] + c3[n3 - 3]) / (2 * h3)
+                - D[0] * (-3 * c2[0] + 4 * c2[1] - c2[2]) / (2 * h2 * x2d_nodes[0])]
+        
+        for i in range(1,n2-1):
+            j = n3 + ej * j # scales up the number of new matrix columns/rows noting that we need extra entries for alpha terms
+
+
+            if c2[i] < cs:  # no reaction
+                
+
+
+
+
+
+
+        # at the last node we impose no H flux out of the domain
+        j = n2 - 1
+        k = n3 + ej * j
+        row += 3 * [k]
+        col += [k - 2 * ej, k - ej, k]
+        val += [-1, 4, -3]
+        # second order differencing
+        b += [(3 * c2[j] - 4 * c2[j - 1] + c2[j - 2])]
